d6e474bd3053c38a35f5f89f00f0905c89abd291
Ahmet BURSALI <65800056+buaht0@users.noreply.github.com>
2021-06-15 01:43:54 +0300

    Update and rename p1-band_name_generator.py to project1-band_name_generator.py

--- project1-band_name_generator.py
+++ project1-band_name_generator.py
@@ -7,5 +7,5 @@ city = input("What's name of the city you grew up in? ")
 pet = input("What's your pet's name? ")
 #4. Combine the name of their city and pet and show them their band name.
 print("Your band name could be " + city + " " + pet + "\n" )
-#5. Make sure the input cursor shows on a new line, see the example at:
-#   https://band-name-generator-end.appbrewery.repl.run/
+
+
