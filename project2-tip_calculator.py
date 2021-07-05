56b4509fa5182b7c0cc95095d6ffb88590918f31
Ahmet BURSALI <65800056+buaht0@users.noreply.github.com>
2021-07-05 17:16:54 +0300

    Update project2-tip_calculator.py

--- project2-tip_calculator.py
+++ project2-tip_calculator.py
@@ -1,5 +1,4 @@
 # Project 2 - Tip calculator
-
 print("Welcome to the tip calculator.")
 total_bill = input("What was the total bill? $")
 percentage_tip = input("What percentage tip would you like to give? 10, 12 or 15?")
