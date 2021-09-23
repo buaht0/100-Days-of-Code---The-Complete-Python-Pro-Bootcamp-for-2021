48da2c6eefe3523974942d26cd1565d43759fc00
Ahmet BURSALI <65800056+buaht0@users.noreply.github.com>
2021-09-23 14:33:04 +0300

    Update and rename main.py to project18-the-hirst-painting.py

--- project18-the-hirst-painting.py
+++ project18-the-hirst-painting.py
@@ -1,3 +1,5 @@
+
+# Project 18 - The Hirst Painting
 import turtle as turtle_module
 import random
 
@@ -34,10 +36,6 @@ for dot_count in range(1, number_of_dots + 1):
         tim.setheading(180)
         tim.forward(500)
         tim.setheading(0)
-
-
-
-
 
 
 screen = turtle_module.Screen()
